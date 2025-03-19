403c859c86488282a34f967ca4ca088bd59164fc
unknown <agusiglesias72@gmail.com>
2025-03-19 14:44:10 -0300

    set up dashboard

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,9 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "react/no-unescaped-entities": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
     },
   },
 ];
