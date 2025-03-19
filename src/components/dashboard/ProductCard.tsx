86ba30a47903a75e8913707fb8367e700bccd00c
unknown <agusiglesias72@gmail.com>
2025-03-19 16:47:08 -0300

    fix cards

--- src/components/dashboard/ProductCard.tsx
+++ src/components/dashboard/ProductCard.tsx
@@ -1,156 +1,147 @@
 import React from 'react';
 import { Card, CardContent } from '@/components/ui/card';
-import { Trash2 } from 'lucide-react';
+import { Trash2, ExternalLink } from 'lucide-react';
 
 // Logo de Mercado Libre como componente SVG
 const MercadoLibreLogo = () => (
-  <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    viewBox="0 0 1000 1000" 
-    className="inline-block w-4 h-4 mr-1.5"
-    aria-hidden="true"
-  >
-    <path d="M418.2 386.4c-8.1 0-14.4 4.6-17.6 12.1h35.1c-3.1-7.5-9.5-12.1-17.5-12.1zm-118.7 19.9c-8 0-14.4 4.6-17.5 12.2h35c-3.1-7.6-9.4-12.2-17.5-12.2zm-58.6-19.9c-17.7 0-30.2 13.1-30.2 31.4 0 18.3 12.5 31.4 30.2 31.4 10.9 0 20.6-5.7 25.7-15l-12.6-7.3c-2.6 4.8-7.5 7.8-13.1 7.8-8 0-13.9-6.6-13.9-16.9 0-10.3 5.8-16.9 13.9-16.9 5.6 0 10.5 3 13.1 7.8l12.6-7.3c-5.1-9.4-14.8-15-25.7-15zm312.9 0c-8.5 0-16.2 3.5-21.7 9.8v-7h-15.6v60.1h16v-32.3c0-9.6 5.3-15.6 13.7-15.6 7.8 0 12.5 5.3 12.5 14.4v33.5h16v-37.7c0-16.8-11.1-25.2-20.9-25.2zm-209.3 0c-18.3 0-31.3 13-31.3 31.4 0 18.4 13 31.4 31.3 31.4 18.4 0 31.3-13 31.3-31.4 0-18.4-12.9-31.4-31.3-31.4zm0 47.7c-8.4 0-14.9-7-14.9-16.3 0-9.3 6.5-16.3 14.9-16.3 8.4 0 14.9 7 14.9 16.3 0 9.3-6.5 16.3-14.9 16.3zm118.7-47.7c-18.1 0-31.4 13-31.4 31.4 0 18.4 13.3 31.4 31.4 31.4s31.3-13 31.3-31.4c0-18.4-13.2-31.4-31.3-31.4zm0 47.7c-8.8 0-15.1-7-15.1-16.3 0-9.3 6.3-16.3 15.1-16.3 8.7 0 15 7 15 16.3 0 9.3-6.3 16.3-15 16.3zm-177.3-47.7c-18.4 0-30.8 13.1-30.8 31.4 0 18.3 12.4 31.4 30.8 31.4 13.1 0 24.4-7.5 28.9-18.9h-17.3c-2.1 3.7-6.3 6-11.6 6-7.4 0-13.2-5-14.5-13.4h46.5v-5.1c0-18.3-12.5-31.4-32-31.4zm208.9 0c-9.5 0-17.7 4.9-22.7 12.9v-10.1h-15.7v60.1h16v-32.3c0-9.4 5.3-15.3 13-15.3 1.9 0 3.7.3 5.3.9l5.3-15.4c-.5-.4-1-.8-1.2-.8z" fill="#4a4a4a"/>
-    <path d="M583.5 429.4c0-22.4-23.6-21.8-23.6-32.2 0-3.5 3.2-6.3 10.1-6.3 6.2 0 11 1.9 11 1.9l1.7-12.5s-3.7-2.5-13.9-2.5c-11.5 0-23.1 6.6-23.1 22.7 0 8.9 6.3 15.6 14.3 20.4 6.6 3.9 8.9 6.5 8.9 10.3 0 4.1-3.3 7.4-9.4 7.4-7.9 0-15.3-3.3-15.3-3.3l-1.8 12.8s6.9 3.7 18.4 3.7c16.8 0 25.7-10.1 25.7-22.4z" fill="#2d3277"/>
-    <path d="M672 379.4c-8.2 0-14.7 3.9-14.7 3.9v-33h-16v89.6h16v-45.3c0-5.3 3.8-10.7 11.5-10.7 8.2 0 13.5 5.3 13.5 16.2v39.8h16v-42.8c-.1-17.8-10.6-17.7-26.3-17.7z" fill="#009ee3"/>
-    <path d="M737.3 386.3c-2.1-.4-4.1-.6-6.2-.6-8.3 0-13.9 6.3-13.9 6.3v-6.3h-15.8v60.1h16v-36.9c0-8.9 6.3-14.7 15.2-14.7 1.9 0 3.6.3 4.7.5l2-8.4h-2z" fill="#0864ee"/>
-  </svg>
+ <svg height="2263" viewBox="1338.7 1396.4 150.1 135.8" width="2500" xmlns="http://www.w3.org/2000/svg"><path d="m1378.1 1504.2h1.4v28h-5.3v-24.6c0-.8 1.7-3.4 3.9-3.4zm29 6.2c-2.6 0-4.6 1.1-6 3.3v-9.5h-.6c-2.6 0-4 1.4-4.7 2.6v-.2 16.1c0 6.3 3.3 9.5 9.9 9.5 6.6-.1 9.8-3.7 9.8-10.7 0-7.3-2.8-10.9-8.4-11.1zm-1.6 17.2c-3 0-4.5-2.2-4.4-6.4.2-4.1 1.6-6.1 4.5-6.1s4.3 2 4.5 6.1c0 4.3-1.6 6.4-4.6 6.4zm13.1-6.4c.2-6.8 3.5-10.6 10.6-10.6h1.6v5.5h-2.5c-3 0-4.3 1.9-4.3 5v11h-5.4zm20.6 5.5c.9.8 2.1 1.2 3.7 1.2 1.1 0 2.1-.3 2.9-.9s1.3-1.2 1.5-1.8h5c-.8 2.5-2 4.2-3.7 5.3s-3.6 1.6-6 1.6c-1.6 0-3.1-.3-4.4-.8s-2.4-1.3-3.3-2.2c-.9-1-1.6-2.1-2.1-3.4s-.7-2.8-.7-4.4c0-1.5.3-3 .8-4.3s1.2-2.5 2.1-3.4c.9-1 2-1.7 3.3-2.3s2.7-.8 4.3-.8c1.7 0 3.3.3 4.6 1s2.4 1.6 3.2 2.7 1.4 2.4 1.8 3.9c.4 1.4.5 3 .4 4.6h-14.9c.2 1.9.7 3.2 1.5 4zm6.4-10.8c-.7-.7-1.7-1.1-3.1-1.1-.9 0-1.7.2-2.3.5s-1.1.7-1.4 1.1c-.4.5-.6.9-.8 1.4s-.2 1-.3 1.4h9.2c-.1-1.5-.6-2.6-1.3-3.3zm-60.4-11.7h5.3v4.8h-5.3zm0 7.2h5.3v20.7h-5.3zm96.7-18.3c-.9 1-2.1 1.6-3.6 1.6s-2.7-.6-3.6-1.6-1.2-2.7-1.2-4.7c0-2.1.4-3.6 1.2-4.6.9-1 2.1-1.6 3.6-1.6s2.7.6 3.6 1.6 1.2 2.7 1.2 4.6-.3 3.5-1.2 4.7zm4.2-12.4c-1.6-2.1-4.3-3.3-7.8-3.3-3.4 0-6.2 1-7.8 3.3-1.6 2.1-2.6 4.7-2.6 7.6 0 3 .9 5.6 2.6 7.6 1.6 2.1 4.3 3.2 7.8 3.2 3.4 0 6.2-1 7.8-3.2 1.6-2.1 2.6-4.7 2.6-7.6.1-2.8-.8-5.4-2.6-7.6m-26 12.4c-.7 1-1.9 1.6-3.4 1.6s-2.7-.6-3.3-1.6c-.7-1.2-1-2.7-1-4.7 0-1.8.3-3.2 1-4.3.7-1.2 1.8-1.8 3.4-1.8 1 0 1.9.3 2.7.9 1.2 1 1.9 3 1.9 5.6-.3 1.8-.5 3.3-1.3 4.3zm6.2-21.8s-5.4-.6-5.4 3.7v5.7c-.6-.9-1.3-1.6-2.4-2.3-.9-.6-2.1-.7-3.3-.7-2.7 0-4.8 1-6.5 3-1.6 1.9-2.4 4.8-2.4 8.4 0 3 .7 5.6 2.4 7.3 1.6 1.9 4.8 2.7 7.6 2.7 9.9 0 9.7-8.4 9.7-8.4zm-26.6 19.3c0 1.8-.6 3-1.5 3.7-1 .6-2.1 1-3.2 1-.7 0-1.3-.1-1.8-.6s-.7-1-.7-1.9c0-1 .4-1.8 1.2-2.3.4-.3 1.3-.6 2.4-.7l1.2-.3c.6-.1 1-.3 1.5-.3.3-.1.7-.3 1-.4zm2.7-11.7c-1.8-.9-3.7-1.3-6-1.3-3.4 0-5.9.9-7.3 2.7-.9 1.2-1.3 2.6-1.5 4.3h5.1c.1-.7.4-1.3.7-1.8.6-.6 1.5-.9 2.7-.9s1.9.1 2.6.4c.6.3.9.9.9 1.8 0 .7-.4 1.2-1.2 1.5-.4.1-1.2.3-2.1.4l-1.8.1c-2.1.3-3.6.7-4.7 1.3-1.9 1-2.9 3-2.9 5.4 0 1.9.6 3.4 1.8 4.5 1.2 1 2.7 1.5 4.6 1.6 11.7.4 11.6-6.2 11.6-7.5v-7.6c.1-2.2-.7-4-2.5-4.9m-26.5 3.3c1.3 0 2.3.4 3 1.2.4.6.7 1.3.7 2.1h5.7c-.3-2.9-1.3-5-3-6-1.6-1.2-3.9-1.6-6.6-1.6-3.2 0-5.7.9-7.5 2.9s-2.7 4.7-2.7 8.1c0 3.2.7 5.6 2.4 7.5 1.6 1.9 4.2 2.9 7.6 2.9s6-1.2 7.8-3.4c1-1.5 1.6-3 1.8-4.7h-5.7c-.1 1-.4 1.9-1 2.6-.6.6-1.5 1-2.9 1-1.8 0-3.2-.9-3.7-2.6-.3-.9-.6-2.1-.6-3.6s.1-2.9.6-3.7c.9-1.8 2.1-2.7 4.1-2.7m-11.9-4.5c-11.9 0-11.1 10.5-11.1 10.5v10.6h5.4v-9.9c0-1.6.1-2.9.6-3.6.7-1.3 2.1-2.1 4.3-2.1h.6c.3 0 .6 0 .9.1v-5.4h-.4c-.1-.2-.1-.2-.3-.2m-26.8 5.5c.7-.7 1.8-1.2 3.2-1.2 1.2 0 2.3.3 3.2 1s1.3 1.8 1.3 3.2h-9.2c.3-1.2.7-2.2 1.5-3zm7.2 9.9c-.1.3-.4.6-.7.7-.7.6-1.8.7-3 .7s-2.1-.1-2.9-.7c-1.3-.7-2.1-2.3-2.1-4.2h14.8c0-1.8 0-3.2-.1-4-.3-1.6-.7-3-1.6-4.2-.9-1.3-2.1-2.4-3.4-3s-3-.9-4.8-.9c-3 0-5.4.9-7.2 2.9-1.8 1.9-2.9 4.6-2.9 8.1 0 3.7 1 6.5 3.2 8.1 2.1 1.6 4.5 2.6 7.2 2.6 3.3 0 5.9-1 7.6-3 1-1 1.6-2.1 1.8-3.2zm-16.2 5.6h-5v-12.3c0-1.2-.3-3.7-3.6-3.7-2.1 0-3.7 1.5-3.7 3.7v12.3h-5v-12.3c0-1.2-.3-3.7-3.6-3.7-2.1 0-3.6 1.5-3.6 3.7v12.3h-5v-12.2c0-5.1 3.3-9 8.5-9 2.6 0 4.7 1 6.2 2.9 1.5-1.8 3.6-2.9 6.2-2.9 5.4 0 8.5 3.7 8.5 9zm93.3-71.2c0-17.1-21.3-31.1-47.6-31.1s-47.6 14-47.6 31.1v1.8c0 18.2 18.6 32.9 47.6 32.9 29.1 0 47.6-14.7 47.6-32.9z" fill="#2d3277"/><path d="m1459.8 1427.5c0 16.1-20.5 29.2-45.7 29.2s-45.7-13.1-45.7-29.2 20.5-29.2 45.7-29.2 45.7 13.1 45.7 29.2z" fill="#ffe600"/><g fill="#fff"><path d="m1398.9 1418.3s-.5.5-.2.9c.7.9 2.9 1.4 5.2.9 1.3-.3 3.1-1.7 4.7-3 1.8-1.4 3.6-2.9 5.4-3.4 1.9-.6 3.1-.3 3.9-.1.9.3 1.9.9 3.6 2.1 3.1 2.3 15.7 13.3 17.9 15.2 1.7-.8 9.5-4.1 20.1-6.5-.9-5.6-4.3-10.8-9.5-15-7.2 3-16.1 4.6-24.8.4 0 0-4.7-2.2-9.4-2.1-6.9.2-9.8 3.1-13 6.3z"/><path d="m1438.9 1432.1c-.1-.1-14.8-12.9-18.1-15.4-1.9-1.4-3-1.8-4.1-2-.6-.1-1.4 0-2 .2-1.5.4-3.6 1.8-5.4 3.2-1.9 1.5-3.6 2.9-5.2 3.2-2.1.5-4.6-.1-5.7-.9-.5-.3-.8-.7-1-1.1-.4-1 .4-1.8.5-1.9l4-4.4 1.4-1.4c-1.3.2-2.5.5-3.7.8-1.5.4-2.9.8-4.3.8-.6 0-3.8-.5-4.4-.7-3.7-1-6.9-2-11.7-4.2-5.7 4.3-9.6 9.6-10.7 15.5.8.2 2.2.6 2.7.7 13 2.9 17 5.9 17.8 6.5.8-.9 1.9-1.4 3.2-1.4 1.4 0 2.7.7 3.5 1.8.7-.6 1.8-1.1 3.1-1.1.6 0 1.2.1 1.9.3 1.5.5 2.2 1.5 2.6 2.4.5-.2 1.1-.4 1.8-.4s1.4.2 2.2.5c2.4 1 2.8 3.4 2.6 5.2h.5c2.9 0 5.2 2.3 5.2 5.2 0 .9-.2 1.7-.6 2.4.8.4 2.7 1.4 4.5 1.2 1.4-.2 1.9-.6 2.1-.9.1-.2.3-.4.1-.6l-3.7-4.1s-.6-.6-.4-.8.6.1.9.3c1.9 1.6 4.1 3.9 4.1 3.9s.2.3 1 .5c.7.1 2 0 2.9-.7.2-.2.5-.4.6-.6.9-1.2-.1-2.4-.1-2.4l-4.3-4.8s-.6-.6-.4-.8.6.1.9.3c1.4 1.1 3.3 3.1 5.1 4.9.4.3 2 1.3 4.1-.1 1.3-.9 1.6-1.9 1.5-2.7-.1-1-.9-1.8-.9-1.8l-5.8-5.9s-.6-.5-.4-.8c.2-.2.6.1.9.3 1.9 1.6 6.9 6.2 6.9 6.2.1 0 1.8 1.3 4-.1.8-.5 1.3-1.2 1.3-2.1.1-1.3-1-2.2-1-2.2z"/><path d="m1410.6 1439.6c-.9 0-1.9.5-2 .5s0-.4.1-.6 1.3-3.8-1.6-5.1c-2.2-1-3.6.1-4 .6-.1.1-.2.1-.2 0 0-.6-.3-2.4-2.3-3-2.8-.9-4.5 1.1-5 1.8-.2-1.6-1.5-2.8-3.2-2.8-1.8 0-3.2 1.4-3.2 3.2s1.4 3.2 3.2 3.2c.9 0 1.6-.3 2.2-.9v.1c-.1.8-.4 3.7 2.6 4.8 1.2.5 2.2.1 3.1-.5.3-.2.3-.1.3.1-.1.7 0 2.3 2.3 3.2 1.7.7 2.7 0 3.3-.6.3-.3.4-.2.4.2.1 2.1 1.9 3.8 4 3.8 2.2 0 4-1.8 4-4s-1.8-4-4-4z"/></g><path d="m1439.5 1430.6c-4.5-3.9-14.9-13-17.8-15.1-1.6-1.2-2.7-1.9-3.7-2.1-.4-.1-1-.3-1.8-.3-.7 0-1.5.1-2.3.4-1.8.6-3.6 2-5.4 3.4l-.1.1c-1.6 1.3-3.3 2.6-4.6 2.9-.6.1-1.1.2-1.7.2-1.4 0-2.7-.4-3.2-1-.1-.1 0-.3.2-.5l4-4.3c3.1-3.1 6-6 12.8-6.2h.3c4.2 0 8.4 1.9 8.9 2.1 4 1.9 8 2.9 12.1 2.9 4.3 0 8.7-1.1 13.3-3.2-.5-.4-1.1-.9-1.6-1.3-4.1 1.8-7.9 2.6-11.7 2.6s-7.6-.9-11.3-2.7c-.2-.1-4.8-2.3-9.7-2.3h-.4c-5.7.1-8.9 2.1-11 3.9-2.1 0-3.9.6-5.5 1-1.4.4-2.7.7-3.9.7h-1.5c-1.4 0-8.4-1.7-13.9-3.9-.6.4-1.1.8-1.7 1.2 5.8 2.4 12.9 4.2 15.1 4.4.6 0 1.3.1 2 .1 1.5 0 2.9-.4 4.4-.8.9-.2 1.8-.5 2.7-.7l-.8.8-4 4.4c-.3.3-1 1.2-.6 2.2.2.4.6.8 1.1 1.2 1 .6 2.7 1.1 4.3 1.1.6 0 1.2-.1 1.7-.2 1.7-.4 3.5-1.8 5.3-3.3 1.5-1.2 3.6-2.7 5.2-3.2.5-.1 1-.2 1.5-.2h.4c1.1.1 2.1.5 4 1.9 3.3 2.5 18 15.3 18.1 15.4 0 0 .9.8.9 2.2 0 .7-.5 1.4-1.2 1.9-.6.4-1.3.6-1.9.6-1 0-1.7-.5-1.7-.5s-5.1-4.6-6.9-6.2c-.3-.3-.6-.5-.9-.5-.2 0-.3.1-.4.2-.3.4 0 .9.4 1.2l5.9 5.9s.7.7.8 1.6c0 1-.4 1.8-1.4 2.4-.7.5-1.4.7-2.1.7-.9 0-1.5-.4-1.7-.5l-.9-.8c-1.5-1.5-3.1-3.1-4.3-4-.3-.2-.6-.5-.9-.5-.1 0-.3 0-.4.2-.1.1-.2.4.1.9.1.2.3.3.3.3l4.3 4.8s.9 1.1.1 2l-.2.2-.4.4c-.7.6-1.7.7-2.1.7h-.6c-.4-.1-.7-.2-.9-.4-.2-.3-2.4-2.4-4.2-3.9-.2-.2-.5-.4-.8-.4-.1 0-.3.1-.4.2-.3.4.2 1 .4 1.2l3.7 4s0 .1-.1.3-.6.6-1.9.8h-.5c-1.4 0-2.8-.7-3.6-1.1.3-.7.5-1.5.5-2.3 0-3-2.4-5.4-5.4-5.4h-.2c.1-1.4-.1-4-2.8-5.1-.8-.3-1.5-.5-2.3-.5-.6 0-1.1.1-1.7.3-.6-1.1-1.5-1.9-2.7-2.3-.7-.2-1.3-.3-2-.3-1.1 0-2.1.3-3 1a4.6 4.6 0 0 0 -3.6-1.7c-1.2 0-2.4.5-3.2 1.3-1.1-.9-5.6-3.7-17.7-6.5-.6-.1-1.9-.5-2.7-.8-.1.6-.2 1.3-.3 2 0 0 2.2.5 2.7.6 12.3 2.7 16.4 5.6 17.1 6.1-.2.6-.3 1.2-.3 1.8 0 2.6 2.1 4.6 4.6 4.6.3 0 .6 0 .9-.1.4 1.9 1.6 3.3 3.5 4 .6.2 1.1.3 1.6.3.3 0 .7 0 1.1-.1.3.9 1.1 2 2.9 2.7.6.3 1.2.4 1.8.4.5 0 1-.1 1.4-.3.8 2 2.8 3.4 5 3.4 1.5 0 2.9-.6 3.9-1.7.9.5 2.7 1.4 4.6 1.4h.7c1.9-.2 2.7-1 3.1-1.5.1-.1.1-.2.2-.3.4.1.9.2 1.5.2 1 0 2-.3 3-1.1 1-.7 1.7-1.7 1.7-2.6.3.1.7.1 1 .1 1 0 2.1-.3 3.1-1 1.9-1.2 2.2-2.9 2.2-3.9.3.1.7.1 1 .1 1 0 2-.3 2.9-.9 1.2-.8 1.9-1.9 2-3.2.1-.9-.2-1.8-.6-2.6 3.2-1.4 10.4-4 19-6 0-.7-.1-1.3-.3-2-10.3 2.2-18 5.5-19.9 6.4zm-28.9 16.7c-2 0-3.6-1.6-3.7-3.6 0-.2 0-.6-.4-.6-.2 0-.3.1-.5.2-.4.4-1 .8-1.8.8-.4 0-.8-.1-1.2-.3-2.1-.9-2.2-2.3-2.1-2.9 0-.2 0-.3-.1-.4l-.1-.1h-.1c-.1 0-.2 0-.4.2-.6.4-1.2.6-1.8.6-.3 0-.7-.1-1-.2-2.8-1.1-2.6-3.7-2.4-4.5 0-.2 0-.3-.1-.4l-.2-.2-.2.2c-.6.5-1.3.8-2 .8-1.6 0-2.9-1.3-2.9-2.9s1.3-2.9 2.9-2.9c1.4 0 2.7 1.1 2.9 2.5l.1.8.4-.7c0-.1 1.2-1.9 3.4-1.8.4 0 .8.1 1.3.2 1.7.5 2 2.1 2 2.7 0 .4.3.4.3.4.1 0 .3-.1.3-.2.3-.3 1-.9 2.1-.9.5 0 1 .1 1.6.4 2.7 1.2 1.5 4.6 1.5 4.7-.2.6-.3.8 0 1h.2c.1 0 .3 0 .5-.1.4-.1.9-.3 1.4-.3 2 0 3.7 1.7 3.7 3.7.1 2.2-1.6 3.8-3.6 3.8z" fill="#2d3277"/></svg>
 );
 
 interface ProductCardProps {
-  id: string;
-  item_id: string;
-  title: string;
-  price: number;
-  regular_amount: number | null;
-  amount: number | null;
-  currency_id: string;
-  thumbnail: string;
-  permalink: string;
-  status: string;
-  last_updated: string;
-  notes?: string | null;
-  brand?: string | null; 
-  onRemove?: (id: string) => Promise<void>;
-  formatCurrency: (amount: number, currency: string) => string;
-  formatDate: (dateString: string) => string;
-}
-
-export default function ProductCard({
-  id,
-  item_id,
-  title,
-  price,
-  regular_amount,
-  amount,
-  currency_id,
-  thumbnail,
-  permalink,
-  status,
-  last_updated,
-  notes,
-  brand,
-  onRemove,
-  formatCurrency,
-  formatDate
-}: ProductCardProps) {
-  return (
-    <Card className="overflow-hidden h-full flex flex-col">
-      <CardContent className="p-0 flex flex-col h-full">
-        <div className="p-4 flex-grow">
-          <div className="flex items-start space-x-4">
-            <div className="w-16 h-16 shrink-0 bg-gray-50 rounded-md flex items-center justify-center overflow-hidden">
-              {thumbnail ? (
-                <img 
-                  src={thumbnail} 
-                  alt={title}
-                  className="w-full h-full object-contain" 
-                />
-              ) : (
-                <div className="w-full h-full bg-gray-100 flex items-center justify-center">
-                  <span className="text-xs text-gray-400">Sin imagen</span>
-                </div>
-              )}
-            </div>
-            <div className="flex-1 min-w-0">
-              <h3 className="text-sm font-medium truncate">{title}</h3>
-              <p className="text-xs text-gray-500">ID: {item_id}</p>
-              
-              <div className="mt-3 h-12"> {/* Altura fija para los precios */}
-                <p className="font-semibold">
-                  {formatCurrency(amount || price, currency_id)}
-                </p>
-                {regular_amount && regular_amount !== amount && (
-                  <p className="text-xs line-through text-gray-500">
-                    {formatCurrency(regular_amount, currency_id)}
-                  </p>
+    id: string;
+    item_id: string;
+    title: string;
+    price: number;
+    regular_amount: number | null;
+    amount: number | null;
+    currency_id: string;
+    thumbnail: string;
+    permalink: string;
+    status: string;
+    last_updated: string;
+    notes?: string | null;
+    brand?: string | null; 
+    onRemove?: (id: string) => Promise<void>;
+    formatCurrency: (amount: number, currency: string) => string;
+    formatDate: (dateString: string) => string;
+  }
+  
+  export default function ProductCard({
+    id,
+    item_id,
+    title,
+    price,
+    regular_amount,
+    amount,
+    currency_id,
+    thumbnail,
+    permalink,
+    status,
+    last_updated,
+    notes,
+    brand,
+    onRemove,
+    formatCurrency,
+    formatDate
+  }: ProductCardProps) {
+    return (
+      <Card className="overflow-hidden h-full flex flex-col">
+        <CardContent className="p-0 flex flex-col h-full">
+          <div className="p-4 flex-grow">
+            <div className="flex items-start space-x-4">
+              <div className="w-16 h-16 shrink-0 bg-gray-50 rounded-md flex items-center justify-center overflow-hidden">
+                {thumbnail ? (
+                  <img 
+                    src={thumbnail} 
+                    alt={title}
+                    className="w-full h-full object-contain" 
+                  />
+                ) : (
+                  <div className="w-full h-full bg-gray-100 flex items-center justify-center">
+                    <span className="text-xs text-gray-400">Sin imagen</span>
+                  </div>
                 )}
               </div>
+              <div className="flex-1 min-w-0">
+                <h3 className="text-sm font-medium truncate">{title}</h3>
+                <p className="text-xs text-gray-500">ID: {item_id}</p>
+                
+                <div className="mt-3 h-12"> {/* Altura fija para los precios */}
+                  <p className="font-semibold">
+                    {formatCurrency(amount || price, currency_id)}
+                  </p>
+                  {regular_amount && regular_amount !== amount && (
+                    <p className="text-xs line-through text-gray-500">
+                      {formatCurrency(regular_amount, currency_id)}
+                    </p>
+                  )}
+                </div>
+              </div>
             </div>
-          </div>
-          
-          {notes && (
-            <div className="mt-2 text-xs bg-gray-50 p-2 rounded-md">
-              <span className="font-medium">Notas:</span> {notes}
-            </div>
-          )}
-          
-          {brand && (
-            <div className="mt-2 text-xs bg-blue-50 p-2 rounded-md">
-              <span className="font-medium">Marca:</span> {brand}
-            </div>
-          )}
-          
-          <div className="mt-3 flex justify-between items-center">
-            <span className="text-xs text-gray-500">
-              Actualizado: {formatDate(last_updated)}
-            </span>
-            <span>
-              <span 
-                className={`inline-block px-2 py-1 rounded-full text-[10px] ${
-                  status === 'active' 
-                    ? 'bg-green-100 text-green-800' 
-                    : status === 'paused'
-                    ? 'bg-yellow-100 text-yellow-800'
-                    : status === 'closed'
-                    ? 'bg-red-100 text-red-800'
-                    : 'bg-gray-100 text-gray-800'
-                }`}
-              >
-                {status === 'active' 
-                  ? 'Activo' 
-                  : status === 'paused'
-                  ? 'En Pausa'
-                  : status === 'closed'
-                  ? 'Finalizada'
-                  : status}
+            
+            {notes && (
+              <div className="mt-2 text-xs bg-gray-50 p-2 rounded-md">
+                <span className="font-medium">Notas:</span> {notes}
+              </div>
+            )}
+            
+            {brand && (
+              <div className="mt-2 text-xs bg-blue-50 p-2 rounded-md">
+                <span className="font-medium">Marca:</span> {brand}
+              </div>
+            )}
+            
+            <div className="mt-3 flex justify-between items-center">
+              <span className="text-xs text-gray-500">
+                Actualizado: {formatDate(last_updated)}
               </span>
-            </span>
+              <span>
+                <span 
+                  className={`inline-block px-2 py-1 rounded-full text-[10px] ${
+                    status === 'active' 
+                      ? 'bg-green-100 text-green-800' 
+                      : status === 'paused'
+                      ? 'bg-yellow-100 text-yellow-800'
+                      : status === 'closed'
+                      ? 'bg-red-100 text-red-800'
+                      : 'bg-gray-100 text-gray-800'
+                  }`}
+                >
+                  {status === 'active' 
+                    ? 'Activo' 
+                    : status === 'paused'
+                    ? 'En Pausa'
+                    : status === 'closed'
+                    ? 'Finalizada'
+                    : status}
+                </span>
+              </span>
+            </div>
           </div>
-        </div>
-        
-        <div className="border-t mt-auto flex">
-          <a 
-            href={permalink} 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="flex-1 flex items-center justify-center py-2.5 text-sm bg-[#ffe600] hover:bg-[#f1d900] text-black font-medium transition-colors"
-          >
-            <MercadoLibreLogo />
-            Ver en ML
-          </a>
           
-          {onRemove && (
-            <button 
-              onClick={() => onRemove(id)}
-              className="flex-1 flex justify-center items-center py-2.5 text-sm text-red-600 hover:bg-red-50 transition-colors border-l"
+          <div className="border-t mt-auto flex">
+            <a 
+              href={permalink} 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="flex-1 flex items-center justify-center py-2.5 text-sm bg-[#ffe600] hover:bg-[#f1d900] text-black font-medium transition-colors"
             >
-              <Trash2 className="h-4 w-4 mr-1" />
-              Eliminar
-            </button>
-          )}
-        </div>
-      </CardContent>
-    </Card>
-  );
-}
+              <MercadoLibreLogo />
+              Ver en ML
+              <ExternalLink className="ml-1.5 h-3.5 w-3.5" />
+            </a>
+            
+            {onRemove && (
+              <button 
+                onClick={() => onRemove(id)}
+                className="flex-1 flex justify-center items-center py-2.5 text-sm text-red-600 hover:bg-red-50 transition-colors border-l"
+              >
+                <Trash2 className="h-4 w-4 mr-1" />
+                Eliminar
+              </button>
+            )}
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
