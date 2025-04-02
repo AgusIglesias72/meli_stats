224ed1c416c768914a6aa1a2534513d0307bf76e
unknown <agusiglesias72@gmail.com>
2025-04-02 02:52:36 -0300

    return to edge function

--- src/lib/googleSheetsClient.ts
+++ src/lib/googleSheetsClient.ts
@@ -1,10 +1,12 @@
-// lib/googleSheetsClient.ts
 import { google } from 'googleapis';
 
 const scopes = ['https://www.googleapis.com/auth/spreadsheets'];
 
 export function getSheetsClient() {
-  const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT!);
+  const credentials = {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  };
 
   const auth = new google.auth.GoogleAuth({
     credentials,
